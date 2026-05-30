f75a3a569ab2f3a06dd36f9c60555f617af33665
gnahiak2 <wangz9096z@gmail.com>
2026-05-30 12:17:41 +0800

    Define BUF_SIZE and implement flash_size_from_jedec function in main.c

--- espdump-firmware/main/main.c
+++ espdump-firmware/main/main.c
@@ -14,4 +14,22 @@
 
 #define UART_NUM UART_NUM_0
 
-#define BUF_SIZE
+#define BUF_SIZE (64 * 1024)
+
+static const char *TAG = "espdump";
+
+uint32_t flash_size_from_jedec(uint8_t cap)
+{
+    switch (cap)
+    {
+        case 0x18: return 16 * 1024 * 1024;
+        case 0x17: return 8 * 1024 * 1024;
+        case 0x16: return 4 * 1024 * 1024;
+        default: return 16 * 1024 * 1024;
+    }
+}
+
+void app_main()
+{
+    
+}
